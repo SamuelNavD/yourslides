73d24919304ec934ae92968a882c020763d6636a
Samuel Navarro <samuel@diezproyectos.com>
2020-07-11 02:51:57 +0200

    Pequeñas mejoras en userRoutes

--- server/routes/user.js
+++ server/routes/user.js
@@ -9,7 +9,6 @@ const router = express.Router();
 const multipart = require('connect-multiparty');
 var multipartMiddleware = multipart({ uploadDir: __dirname + '/../uploads'});
 
-router.get('/private', auth, (req, res)=>{res.status(200).send({ message: "Estás dentro" });});
 router.post('/signup', multipartMiddleware, userCtrl.signUp);
 router.post('/signin', multipartMiddleware, userCtrl.signIn);
 router.put('/update', multipartMiddleware, userCtrl.updateUser);
