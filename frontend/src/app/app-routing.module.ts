a2193d6957e1e99b75bb5a6eb142e1d020bfbf96
Samuel Navarro <samuel@diezproyectos.com>
2020-07-28 01:51:59 +0200

    Primeros pasos del dashboard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SiteLayoutComponent } from './components/_layouts/site-layout/site-layout.component';
+import { PanelLayoutComponent } from './components/_layouts/panel-layout/panel-layout.component';
 
-const routes: Routes = [];
+import { HomePanelComponent } from './components/panel/home-panel/home-panel.component';
+
+const routes: Routes = [
+  {
+    path: 'panel',
+    component: PanelLayoutComponent,
+    children: [
+      { path: '', component: HomePanelComponent}
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
+import { SiteLayoutComponent } from './components/_layouts/site-layout/site-layout.component';
+import { PanelLayoutComponent } from './components/_layouts/panel-layout/panel-layout.component';
+import { HomePanelComponent } from './components/panel/home-panel/home-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    SiteLayoutComponent,
+    PanelLayoutComponent,
+    HomePanelComponent
   ],
   imports: [
     BrowserModule,
