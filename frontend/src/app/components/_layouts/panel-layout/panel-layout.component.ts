4dec9d116a50f0f07aedcca57304a904adad94c2
Samuel Navarro <samuel@diezproyectos.com>
2020-08-04 18:41:11 +0200

    Mejoras en el layout del panel

--- frontend/src/app/components/_layouts/panel-layout/panel-layout.component.ts
+++ frontend/src/app/components/_layouts/panel-layout/panel-layout.component.ts
@@ -19,4 +19,36 @@ export class PanelLayoutComponent implements OnInit {
     this.user = this._userService.user;
   }
 
+  animateCSS(element: string, animation: string, prefix: string = 'animate__') {
+    // We create a Promise and return it
+    return new Promise((resolve, reject) => {
+      const animationName = `${prefix}${animation}`;
+      const node = document.querySelector(element);
+
+      node.classList.add(`${prefix}animated`, animationName);
+
+      // When the animation ends, we clean the classes and resolve the Promise
+      function handleAnimationEnd() {
+        node.classList.remove(`${prefix}animated`, animationName);
+        node.removeEventListener('animationend', handleAnimationEnd);
+
+        resolve('Animation ended');
+      }
+
+      node.addEventListener('animationend', handleAnimationEnd);
+    });
+  }
+
+  toggleMenu(element: string, animation_in: string, animation_out: string) {
+    const node = document.querySelector(element);
+    if (node.classList.contains('open')) {
+      this.animateCSS(element, animation_out).then(() => {
+        node.classList.remove('open');
+      });
+    } else {
+      node.classList.add('open');
+      this.animateCSS(element, animation_in).then(() => {
+      });
+    }
+  }
 }
